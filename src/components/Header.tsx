adfdc44bcb466fe06c10f08e3ae3a57b42ddde10
WazeerIqbal <iqbalrasool1919@gmail.com>
2024-08-01 15:30:54 +0500

    cv-update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,6 +14,7 @@ const Header: React.FC<HeaderProps> = ({ scrollToSection }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [isLightModeText, setIsLightModeText] = useState("Light Mode");
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
    
   useEffect(() => {
     const theme = localStorage.getItem('color-theme');
@@ -45,6 +46,7 @@ const Header: React.FC<HeaderProps> = ({ scrollToSection }) => {
   return (
     <div>
       <header className="fixed top-0 left-0 w-full z-50">
+
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="/" className="flex items-center">
@@ -64,7 +66,7 @@ const Header: React.FC<HeaderProps> = ({ scrollToSection }) => {
 
               {/* Hamburger for mobile */}
               <button onClick={toggleMenu} data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded={isMenuOpen}>
-                <span className="sr-only">Open main menu</span>
+                {/* <span className="sr-only">Open main menu</span> */}
                 {isMenuOpen ? (
                   <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
@@ -75,8 +77,9 @@ const Header: React.FC<HeaderProps> = ({ scrollToSection }) => {
                   </svg>
                 )}
               </button>
-
+                
               {/* Toggle button */}
+              {/* It will show two svgs  */}
               <button onClick={toggleTheme} id="theme-toggle" type="button" className="text-gray-500 dark:text-gray-400 border white:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
                 {isDarkMode ? (
                   <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
