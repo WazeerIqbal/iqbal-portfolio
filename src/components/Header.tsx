4c3e69ec83e7325b1eb24dc0d8bd6d027275ef15
WazeerIqbal <iqbalrasool1919@gmail.com>
2024-08-21 21:14:51 +0500

    cv-update1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -114,7 +114,7 @@ const Header: React.FC<HeaderProps> = ({ scrollToSection }) => {
                   <Link to="/about" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About</Link>
                 </li>
                 <li>
-                  <a href="/IQBAL-cv.pdf" download>
+                  <a href="/iqbal-cv-dk.pdf" download>
                     <button className="block py-2 pr-4 pl-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm lg:px-5 lg:py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                       Download CV &#x1F4E5;&nbsp;&nbsp;
                     </button>
